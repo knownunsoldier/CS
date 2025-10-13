fcbe16e3b3dccc67142056ffa7f98a76fbbc29cf
knownunsoldier <7thbakerboy@gmail.com>
2025-10-13 13:53:39 -0400

    Update 1.2.4.py

--- 1.2.4.py
+++ 1.2.4.py
@@ -1,1 +1,39 @@
-import sys, pygame
+#-----import statements-----
+import turtle
+import random
+
+
+
+#-----initialize turtle-----
+turtle.colormode(255)
+win = turtle.Screen()
+spiral = turtle.Turtle()
+
+#-----game configuration----
+win.setup(600,600)      #sets the overall dimensions of the screen
+#win.bgcolor(input("Pick any color you want"))  #background color of window
+win.bgcolor(255, 0, 0)  #background color of window
+
+
+spiral.pencolor(0, 0, 0)
+walls=20
+wall_length=15
+
+#-----game functions--------
+    #spiral
+def makeSpiral():
+    spiral.pendown
+    spiral.goto(0, 0)
+    spiral.setheading(0)
+    for i in range(walls):
+        spiral.forward(wall_length*(1+(i)))
+        spiral.right(90)
+        spiral.forward(wall_length*(1+(i)))
+        spiral.right(90)
+        
+
+
+#-----events / function calls----------------
+makeSpiral()
+
+win.mainloop()
