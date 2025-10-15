b6cd2fa1d794ba98f8f685833e1541184b84ed74
knownunsoldier <7thbakerboy@gmail.com>
2025-10-15 13:41:31 -0400

    Update 1.2.4.py

--- 1.2.4.py
+++ 1.2.4.py
@@ -1,44 +1,35 @@
 #-----import statements-----
 import turtle
 import random
-
-
-
 #-----initialize turtle-----
 turtle.colormode(255)
-win = turtle.Screen()
-spiral = turtle.Turtle()
+win=turtle.Screen()
+spiral=turtle.Turtle()
 spiral.pensize(5)
 spiral.speed(0)
-
-
-#-----game configuration----
-win.setup(600,600)      #sets the overall dimensions of the screen
-#win.bgcolor(input("Pick any color you want"))  #background color of window
-win.bgcolor(255, 255, 255)  #background color of window
-
-
 spiral.pencolor(0, 0, 0)
+#-----game configuration----
+win.setup(600,600)#sets the overall dimensions of the screen
+#rwin.bgcolor(input("Pick any color you want"))  #background color of window 
+win.bgcolor(255, 255, 255)  #background color of window
 walls=20
-wall_length=20
-
+path_width=20
 #-----game functions--------
-    #spiral
-def makeSpiral():
-    global wall_length
+def makeSpiral():       #spiral
+    global path_width
+    wall_length=20
     spiral.goto(0, 0)
     spiral.setheading(90)
     for i in range(walls):
         spiral.forward(wall_length)
-        spiral.right(90)
+        spiral.left(90)
         spiral.forward(wall_length)
-        spiral.right(90)
-        wall_length+=20
-        
-
-
+        spiral.left(90)
+        wall_length+=path_width
+def go():
+    win.tracer(0)
+    makeSpiral()
+    win.update()
 #-----events / function calls----------------
-win.tracer(0)
-makeSpiral()
-win.update()
+go()
 win.mainloop()
