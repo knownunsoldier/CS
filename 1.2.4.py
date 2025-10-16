b3beadf7a4c3b202583e0c4ed69f11aa5f7f853f
knownunsoldier <7thbakerboy@gmail.com>
2025-10-16 13:46:12 -0400

    Update 1.2.4.py

--- 1.2.4.py
+++ 1.2.4.py
@@ -6,8 +6,9 @@ turtle.colormode(255)
 win=turtle.Screen()
 spiral=turtle.Turtle()
 spiral.pensize(5)
-spiral.speed(1)
+spiral.speed(0)
 spiral.pencolor(0, 0, 0)
+spiral.penup()
 #-----game configuration----
 win.setup(600,600)                                  #sets the overall dimensions of the screen
 win.bgcolor(255, 255, 255)                          #background color of window
@@ -18,24 +19,34 @@ door_width=20
 #-----game functions--------
 def makeSpiral():                                   
     global path_width
-    wall_length=60
+    global wall_length
+    wall_length=70
     spiral.goto(0, 0)
+    global i
     for i in range(walls):
+        global l
         for l in range(2):
-            spiral.left(90)
-            spiral.forward(10)
-            spiral.penup()
-            spiral.forward(door_width)
-            spiral.pendown()
+            if i==0 and l==0:
+                spiral.penup()
             spiral.forward(40)
+            spiral.left(90)
+            makeDoor()
 
+            #makeBarrier()
+            spiral.pendown()
             spiral.right(90)
             spiral.forward(path_width)
             spiral.forward(-path_width)
             spiral.left(90)
             spiral.forward(wall_length-(door_width+50))
         wall_length+=path_width
-def go():                                           
+def makeDoor():
+    spiral.forward(10)
+    spiral.penup()
+    if i==0 and l==0:
+        spiral.pendown()
+    spiral.forward(door_width)
+def go():
     makeSpiral()
     win.tracer(0)
     win.update()
