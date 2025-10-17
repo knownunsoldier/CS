3dfe6803d7ad1cdc1b0302c21a3321a8d72f866e
knownunsoldier <7thbakerboy@gmail.com>
2025-10-17 10:48:33 -0400

    Update 1.2.4.py

--- 1.2.4.py
+++ 1.2.4.py
@@ -16,39 +16,50 @@ win.bgcolor(255, 255, 255)                          #background color of window
 walls=20
 path_width=20
 door_width=20
+wall_length=70
 #-----game functions--------
 def makeSpiral():                                   
     global path_width
     global wall_length
-    wall_length=70
-    spiral.goto(0, 0)
+    spiral.goto(40, 0)
     global i
+    spiral.penup() 
     for i in range(walls):
-        global l
-        for l in range(2):
-            if i==0 and l==0:
-                spiral.penup()
-            spiral.forward(40)
-            spiral.left(90)
-            makeDoor()
+        spiral.left(90) 
+        whichfirst=random.randint(0, 1) 
+        half = (wall_length-(door_width+(path_width*2)))/2
+        distance_one = random.randint(0, int(half))
+        distance_two = random.randint(0, int(half))
+        spiral.forward(distance_one)
+        if whichfirst==0:
+            makeDoor() 
+        else:
             makeBarrier()
-        wall_length+=path_width
+        spiral.forward(distance_two)
+        if whichfirst==0:
+            makeBarrier() 
+        else:
+            makeDoor()
+        spiral.forward(wall_length-(distance_one+distance_two))
+        if i%2==1:
+            wall_length+=path_width
 
 def makeDoor():
     spiral.pendown()
-    if i==0 and l==0: #is it the first loop?
+    if i==0: #is it the first loop?
         spiral.penup()
     spiral.forward(10)
     spiral.penup()
     spiral.forward(door_width) #draw door section
     spiral.pendown()
 def makeBarrier():
-    spiral.pendown()
+    if i>walls-5: 
+        spiral.penup()
     spiral.right(90)
     spiral.forward(path_width)
     spiral.forward(-path_width)
     spiral.left(90)
-    spiral.forward(wall_length-(door_width+50))
+    spiral.pendown()
 def go():
     makeSpiral()
     win.tracer(0)
