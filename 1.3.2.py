825b20f9547a024164bc26e3d705c4bac0a97e9b
knownunsoldier <7thbakerboy@gmail.com>
2025-10-21 13:53:51 -0400

    whole bunchashi

--- 1.3.2.py
+++ 1.3.2.py
@@ -1,6 +1,6 @@
+
+
 '''
-DAY 1
- - import libraries + init()
  - CONSTANTS + VARIABLES
  - Setup WIN/Screen
  - setup main game loop
@@ -22,28 +22,73 @@ DAY2
  - Reset to top of screen when jump off platform
 '''
 ##---------- Imports and Setup
-
+import pygame
+pygame.init()
 
 ##-----------WINDOW/SCREEN
+width = 800
+height = 400
+
+win = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+
+
 
 
 ##-----------VARIABLES
+FPS = 60
+SPRITEWH = 70
 
+gray = (49, 54, 56)
+orange = (240, 157, 81)
 
+ghost_x = 300
+ghost_y = 100
 
+spriteFaceRight = True
+GHOSTimg = pygame.image.load('ghost.gif')
+GHOSTimgL = pygame.transform.scale(GHOSTimg, (SPRITEWH, SPRITEWH))
 ##-----------IMAGES
 
 
 
 ##-----------PLATFORMS
+platforms = [
+    pygame.Rect(100, 300, 400, 50),
+    pygame.Rect(100, 250, 50, 50),
+    pygame.Rect(450, 250, 300, 50)
+    ]
 
 
 
 ##-----------FUNCTIONS
-
-
+def update_screen():
+    #fill background
+    win.fill(gray)
+    #draw platforms
+    for p in platforms:
+        pygame.draw.rect(win, orange, p)
+    pygame.display.update()
 
 ##-----------UPDATE SCREEN
 
 
 ##-----------MAINLOOP
+def go():
+    running = True
+    while running:
+        clock.tick(FPS) #reuglate loop speed
+        keys_pressed = pygame.key.get_pressed()
+        #print(keys_pressed)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    print("you quit")
+                    running = False
+                    pygame.quit()
+        update_screen()
+
+go()
