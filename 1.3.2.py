9c2cfeb09c7a084f3b0fc1f925d9e5c3013f6fcf
knownunsoldier <7thbakerboy@gmail.com>
2025-10-27 13:36:20 -0400

    organize outline

--- 1.3.2.py
+++ 1.3.2.py
@@ -1,8 +1,24 @@
 '''
-Backlog 
 DAY 1 
-- import libraries - create our window / GUI - setup main game loop - while running = True. - odate sreen function 
-- get 3 images onto window - create rectangles to control each image - move pac w/ arrows 
-DAY 2 - move pac w/ arrows - keep Pao on left side - create ghost path movement - create bullets 
-DAY 3 - move bullets - determine bullet collisions - display score on screen - determine win condition - add Break and Restart - display Winner message - start game when ENTER KEY clicked 
+- import libraries 
+- create our window / GUI
+- setup main game loop
+- while running = True.
+- update sreen function 
+- get 3 images onto window
+- create rectangles to control each image
+- move pac w/ arrows 
+DAY 2
+- move pac w/ arrows
+- keep Pac on left side
+- create ghost path movement
+- create bullets 
+DAY 3
+- move bullets
+- determine bullet collisions
+- display score on screen
+- determine win condition
+- add Break and Restart
+- display Winner message
+- start game when ENTER KEY clicked 
 '''
